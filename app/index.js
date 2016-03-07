e8fac790a69bcd7e7cd725319466d1d9aef82f83
James Kusachi <jamesku@zillow.com>
2016-03-07 14:09:21 -0800

    starting gitty

---
+++ app/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router, hashHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import routes from './routes';
+import configureStore from './store/configureStore';
+import './app.global.css';
+
+const store = configureStore();
+const history = syncHistoryWithStore(hashHistory, store);
+
+render(
+  <Provider store={store}>
+    <Router history={history} routes={routes} />
+  </Provider>,
+  document.getElementById('root')
+);
+
+if (process.env.NODE_ENV !== 'production') {
+  // Use require because imports can't be conditional.
+  // In production, you should ensure process.env.NODE_ENV
+  // is envified so that Uglify can eliminate this
+  // module and its dependencies as dead code.
+  // require('./createDevToolsWindow')(store);
+}
